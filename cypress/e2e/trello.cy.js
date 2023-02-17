adb8f150e02238c862401c95b5f90c606816493c
NataliaFiryn <natalia.firyn@itmagination.com>
2023-02-17 14:07:56 +0100

    first API tests

--- cypress/e2e/trello.cy.js
+++ cypress/e2e/trello.cy.js
@@ -1,5 +1,53 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('API tests', () => {
+  
+  it('Get all boards', () => {
+    cy.request({
+      url: 'https://api.trello.com/1/members/me/boards',
+      method: 'GET',
+      qs: {
+        'key':'', 
+        'token': ''
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+  it('Create a board', () => {
+    cy.request({
+      url: 'https://api.trello.com/1/boards/',
+      method: 'POST',
+      qs: {
+        'name': 'API Board',
+        'prefs_permissionLevel': 'public',
+        'key':'', 
+        'token': ''
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).to.have.property('id')
+      //cy.wrap(response.body.id).as('boardID')
+     const boardID = response.body.id
+     cy.wrap(boardID).as('boardID')
+    })
+  // })
+  //it('Create a list', () => {
+    cy.get('@boardID').then((boardID) => {
+    cy.request({
+      url: 'https://api.trello.com/1/lists',
+      method: 'POST',
+      qs: {
+        'name': 'TODO List',
+        'idBoard': boardID,
+        'key':'', 
+        'token': ''
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).to.have.property('id')
+      const boadrID = response.body.id
+      cy.wrap(boadrID).as('TODOlistID')
+    })
   })
 })
+})
+
